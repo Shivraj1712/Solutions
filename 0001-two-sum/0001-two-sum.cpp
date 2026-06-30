140526dc507f46b74676ac40d0a193866e8a05f8
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2026-06-30 09:42:30 +0530

    Time: 0 ms (100%), Space: 14.8 MB (54.71%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        for(int i = 0 ; i < nums.size() ; ++i){
+            int val = target - nums[i];
+            if(map.find(val) != map.end()) return {map[val],i};
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
